3619b9b1c5c89a832f64455638054321415ca706
jint <stbhg5@gmail.com>
2026-08-21 11:04:53 +0900

    클래스 생성

---
+++ springmvc/src/main/java/hello/springmvc/basic/request/RequestParamController.java
@@ -0,0 +1,9 @@
+package hello.springmvc.basic.request;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+
+@Slf4j
+@Controller
+public class RequestParamController {
+}
